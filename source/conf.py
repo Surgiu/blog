ceb494bfd6c6cf75bdeed4215243c993a43695a1
qijun han <12212635@mail.sustech.edn.cn>
2024-05-28 00:13:03 +0800

    change md

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    'recommonmark',
     'sphinx_markdown_tables'
 ]
 
